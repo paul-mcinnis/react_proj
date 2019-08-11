34d2ebfe44fbbb54c0c4f8c41a811bd7146d5bc3
Paul McInnis <mcinnis.paul@gmail.com>
2019-08-11 12:58:50 -0500

    added styling for page not found scene

--- src/scenes/PageNotFound/PageNotFound.js
+++ src/scenes/PageNotFound/PageNotFound.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 export default class PageNotFound extends React.Component {
   render() {
-    return <h1>Page Not Found page</h1>;
+    return (
+      <div class="container">
+        <div class="row">
+          <div id="notfoundtxt" class="col-md">
+            <h1>Sorry</h1>
+            <h2>We can't find that page</h2>
+            <Link to="/">
+              Would you like to go the the
+              <strong>home page?</strong>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
