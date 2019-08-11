15245870d1431fcabe9bf8294d0537cbc9e3ee01
Paul McInnis <mcinnis.paul@gmail.com>
2019-08-11 12:41:03 -0500

    router now only renders first match

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { About, Contact, Home, PageNotFound, Projects } from "./scenes";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contactUs" component={Contact} />
-        <Route path="*" component={PageNotFound} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contactUs" component={Contact} />
+          <Route component={PageNotFound} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
