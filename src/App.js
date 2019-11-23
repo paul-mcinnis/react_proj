f186c8d1bf3e4bbadb24c6cb60a94dd119932502
Paul McInnis <mcinnis.paul@gmail.com>
2019-11-22 20:02:43 -0800

    added basic google analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import ReactGA from "react-ga";
 import React from "react";
 import "./App.css";
 import Router from "./Router";
+
+// Initialize google analytics
+ReactGA.initialize("UA-153149185-1");
+ReactGA.pageview("/homepage");
 
 function App() {
   return (
